8c1ff42c2758e613103a3b74c832dc47e8a13266
baundx <erdebaund.dev@gmail.com>
2026-05-02 21:18:40 +0700

    fix: add framer-motion, set light theme default

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -9,8 +9,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
     <JotaiProvider>
       <ModeThemeProvider
         attribute="class"
-        defaultTheme="system"
-        enableSystem
+        defaultTheme="light"
         disableTransitionOnChange
       >
         <ChartThemeProvider>{children}</ChartThemeProvider>
